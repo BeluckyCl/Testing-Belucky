9103c7247d3d9df439569205771051caf34381cb
contacto@belucky.cl <odoo@ampd1.odoo.com>
2021-03-29 20:07:15 +0000

    Controller

--- l10n_cl_cert/models/models.py
+++ l10n_cl_cert/models/models.py
@@ -54,6 +54,7 @@ class AccountMove(models.Model):
         dte_final = dte_signed.encode('iso-8859-1')
         # _logger.info('Despues de Sign')
         _logger.info('Envío DTE: {}'.format(dte_final))
+        return (dte_final)
 
 # class l10n_cl_cert(models.Model):
 #     _name = 'l10n_cl_cert.l10n_cl_cert'
