82bdc3c4d28ae78e5bda855cd46959b8a816a8c9
contacto@belucky.cl <odoo@ampd1.odoo.com>
2021-04-06 16:05:55 +0000

    fix

--- l10n_cl_cert/models/.ipynb_checkpoints/models-checkpoint.py
+++ l10n_cl_cert/models/.ipynb_checkpoints/models-checkpoint.py
@@ -66,7 +66,7 @@ class AccountMove(models.Model):
     def _xml_libro_signed(self):
         company = self.env["res.company"].search([])[0]
         digital_signature = company._get_digital_signature(user_id=self.env.user.id)
-        libro_venta = bytes(base64.b64decode(company.x_libro_venta).decode("utf-8"), 'utf-8')
+        libro_venta = base64.b64decode(company.x_libro_venta) #.decode("utf-8")
         libro_signed = self._sign_full_xml(
             libro_venta, digital_signature, 'EnvioLibro',
             'libro',
